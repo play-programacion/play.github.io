1a6dfe71da21fe96d85d85bc06a906f1ab194c0c
Juan Andreoli <play_pc@outlook.com>
2023-02-28 01:21:01 -0300

    first commit

---
+++ proyecto.js
@@ -0,0 +1,28 @@
+document.getElementById("boton").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="1165487687";
+}
+document.getElementById("mail").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="play_pc@outlook.com";
+}
+document.getElementById("direccion").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="Avenida Nicolás Avellaneda 1754, San Fernando, Buenos Aires, Argentina"
+}
+document.getElementById("nacimiento").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="02-03-1979"
+}
+document.getElementById("dni").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="27217535"
+}
+document.getElementById("civil").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="Casado"
+}
+document.getElementById("nacionalidad").onclick=function()  {
+    console.log("Capturamos el eveno clic")
+    document.getElementById("demo").innerHTML="Argentino"
+}
